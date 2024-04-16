64d725815b5abdf84b8f44120ce8a087a6dc254b
Quentin Mowat-Amiet <qmowatamiet@iese.edu>
2024-04-16 13:02:36 +0200

    feat(analysis): Add main analysis modules.
    Add main, config, and utilities for analysis.

---
+++ analysis/main.py
@@ -0,0 +1,131 @@
+from sentence_transformers import SentenceTransformer
+
+from datetime import timedelta
+from time import process_time
+
+from database import get_all_reviews, update_reviews_scores
+from utils import (
+    get_average_embedding,
+    get_similarity_scores,
+)
+from config import (
+    MODEL_PATH,
+    MODEL,
+    POSITIVE_ATTRIBUTES,
+    CENTRALIZED_REVIEWS,
+    DECENTRALIZED_REVIEWS,
+    SECRETIVE_REVIEWS,
+    TRANSPARENT_REVIEWS,
+    HIERARCHICAL_REVIEWS,
+    NON_HIERARCHICAL_REVIEWS,
+    FORMAL_REVIEWS,
+    INFORMAL_REVIEWS,
+    STAGNATING_REVIEWS,
+    INNOVATIVE_REVIEWS,
+    RISK_AVERSE_REVIEWS,
+    RISK_TAKING_REVIEWS,
+)
+
+
+def main():
+
+    reviews = get_all_reviews()
+
+    print(f"\nTotal reviews: {len(reviews)}\n")
+
+    # Create a dictionary with review_id as the key and review_text as the value
+    reviews_dict = {review.review_id: review.review_text for review in reviews}
+
+    # Import model
+    model = SentenceTransformer(MODEL_PATH)
+
+    # Generate embeddings for both extremes of the structural aspects
+    centralized_embeddings = model.encode(
+        CENTRALIZED_REVIEWS, normalize_embeddings=True
+    )
+    decentralized_embeddings = model.encode(
+        DECENTRALIZED_REVIEWS, normalize_embeddings=True
+    )
+    secretive_embeddings = model.encode(SECRETIVE_REVIEWS, normalize_embeddings=True)
+    transparent_embeddings = model.encode(
+        TRANSPARENT_REVIEWS, normalize_embeddings=True
+    )
+    hierarchical_embeddings = model.encode(
+        HIERARCHICAL_REVIEWS, normalize_embeddings=True
+    )
+    non_hierarchical_embeddings = model.encode(
+        NON_HIERARCHICAL_REVIEWS, normalize_embeddings=True
+    )
+    formal_embeddings = model.encode(FORMAL_REVIEWS, normalize_embeddings=True)
+    informal_embeddings = model.encode(INFORMAL_REVIEWS, normalize_embeddings=True)
+    stagnating_embeddings = model.encode(STAGNATING_REVIEWS, normalize_embeddings=True)
+    innovative_embeddings = model.encode(INNOVATIVE_REVIEWS, normalize_embeddings=True)
+    risk_averse_embeddings = model.encode(
+        RISK_AVERSE_REVIEWS, normalize_embeddings=True
+    )
+    risk_taking_embeddings = model.encode(
+        RISK_TAKING_REVIEWS, normalize_embeddings=True
+    )
+
+    # Get average embeddings for attribute extremes
+    centralized_average = get_average_embedding(centralized_embeddings)
+    decentralized_average = get_average_embedding(decentralized_embeddings)
+    secretive_average = get_average_embedding(secretive_embeddings)
+    transparent_average = get_average_embedding(transparent_embeddings)
+    hierarchical_average = get_average_embedding(hierarchical_embeddings)
+    non_hierarchical_average = get_average_embedding(non_hierarchical_embeddings)
+    formal_average = get_average_embedding(formal_embeddings)
+    informal_average = get_average_embedding(informal_embeddings)
+    stagnating_average = get_average_embedding(stagnating_embeddings)
+    innovative_average = get_average_embedding(innovative_embeddings)
+    risk_averse_average = get_average_embedding(risk_averse_embeddings)
+    risk_taking_average = get_average_embedding(risk_taking_embeddings)
+
+    # Start the multi-process pool on all available CUDA devices
+    pool = model.start_multi_process_pool()
+
+    start_time = process_time()
+
+    # Generate embeddings for all reviews
+    review_embeddings = model.encode_multi_process(
+        reviews_dict.values(), pool
+    )  # normalize_embeddings=True
+
+    end_time = process_time()
+
+    # Optional: Stop the processes in the pool
+    model.stop_multi_process_pool(pool)
+
+    # Print run time
+    elapsed_time = timedelta(seconds=(end_time - start_time))
+    print(f"\n{MODEL} model ran in: {elapsed_time}\n")
+
+    # Get similarity scores for review embeddings
+    similarity_scores_dict = get_similarity_scores(
+        embeddings=review_embeddings,
+        identifiers=reviews_dict.keys(),
+        positive_vectors=[
+            centralized_average,
+            secretive_average,
+            hierarchical_average,
+            formal_average,
+            stagnating_average,
+            risk_averse_average,
+        ],
+        negative_vectors=[
+            decentralized_average,
+            transparent_average,
+            non_hierarchical_average,
+            informal_average,
+            innovative_average,
+            risk_taking_average,
+        ],
+        positive_attributes=POSITIVE_ATTRIBUTES,
+    )
+
+    # Update reviews with similarity scores
+    update_reviews_scores(reviews, similarity_scores_dict)
+
+
+if __name__ == "__main__":
+    main()
---
+++ analysis/utils.py
@@ -0,0 +1,126 @@
+from dask.dataframe import DataFrame as DaskDataFrame
+from sqlalchemy.engine import Row
+from pandas import DataFrame
+from numpy import dot, array, sum
+from numpy.linalg import norm
+
+from typing import List, Dict
+
+from database import Review
+
+
+def to_dask_dataframe(reviews: List[Review], npartitions: int = 10) -> DaskDataFrame:
+    """
+    Convert list of reviews to Dask DataFrame.
+    """
+    reviews_dict = [
+        {
+            "review_id": review.review_id,
+            "employer_id": review.employer_id,
+            "review_text": review.review_text,
+        }
+        for review in reviews
+    ]
+
+    df_reviews = DataFrame(reviews_dict)
+
+    return DaskDataFrame.from_pandas(df_reviews, npartitions=npartitions)
+
+
+def get_average_embedding(embeddings: List[List[float]]) -> list:
+    """
+    Get average embedding for group of related embeddings.
+    """
+    return [
+        sum(dim_values) / len(dim_values)
+        for dim_values in list(map(list, zip(*embeddings)))
+    ]
+
+
+def get_similarity(
+    vector: List[float], pos_vector: List[float], neg_vector: List[float]
+) -> float:
+    """
+    Get similarity measure from (Viashima and Samila, 2022).
+    """
+    vector_a, pos_vector_a, neg_vector_a = (
+        array(vector),
+        array(pos_vector),
+        array(neg_vector),
+    )
+    cos_sim = (dot(vector_a - neg_vector_a, pos_vector_a - neg_vector_a)) / (
+        norm(vector_a - neg_vector_a) * norm(pos_vector_a - neg_vector_a)
+    )
+    term = dot(cos_sim, norm(pos_vector_a - neg_vector_a)) / norm(
+        pos_vector_a - neg_vector_a
+    )
+
+    return term - 0.5
+
+
+def cosine_difference(
+    vector: List[float], pos_vector: List[float], neg_vector: List[float]
+) -> float:
+    """
+    Get cosine similarities for both pairs and take the difference.
+    """
+    vector_a, pos_vector_a, neg_vector_a = (
+        array(vector),
+        array(pos_vector),
+        array(neg_vector),
+    )
+    cos_sim_pos = dot(vector_a, pos_vector_a) / (
+        (norm(vector_a)) * (norm(pos_vector_a))
+    )
+    cos_sim_neg = dot(vector_a, neg_vector_a) / (
+        (norm(vector_a)) * (norm(neg_vector_a))
+    )
+
+    return cos_sim_pos - cos_sim_neg
+
+
+def get_similarity_scores(
+    embeddings: List[List[float]],
+    identifiers: List[int],
+    positive_vectors: List[List[float]],
+    negative_vectors: List[List[float]],
+    positive_attributes: List[str],
+) -> Dict[int, Dict[str, float]]:
+    """
+    Generate similarity scores dictionary for each document's embedding and each attribute's embedding.
+    """
+    return {
+        id: {
+            spec: get_similarity(vec, pos, neg)
+            for pos, neg, spec in zip(
+                positive_vectors, negative_vectors, positive_attributes
+            )
+        }
+        for id, vec in zip(identifiers, embeddings)
+    }
+
+
+def get_cosdiff_scores(
+    embeddings: List[List[float]],
+    identifiers: List[int],
+    pos_vectors: List[List[float]],
+    neg_vectors: List[List[float]],
+    pos_attributes: List[str],
+) -> Dict[int, Dict[str, float]]:
+    """
+    Generate cosine difference scores dictionary for each document's embedding and each attribute's embedding.
+    """
+    return {
+        id: {
+            spec: cosine_difference(vec, pos, neg)
+            for pos, neg, spec in zip(pos_vectors, neg_vectors, pos_attributes)
+        }
+        for id, vec in zip(identifiers, embeddings)
+    }
+
+
+def normalize_embeddings(embeddings: List[List[float]]) -> List[List[float]]:
+    """
+    Get normalized embeddings.
+    """
+    return [vector / norm(vector) for vector in embeddings]
